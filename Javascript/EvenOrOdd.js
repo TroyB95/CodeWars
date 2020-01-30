90f9e1a36e27676d1e1d787d67246e5c70b6210f
Troy Budd <troy_budd95@msn.com>
2020-01-30 09:41:00 +0000

    TB - Corrected function name to follow convention

--- Javascript/EvenOrOdd.js
+++ Javascript/EvenOrOdd.js
@@ -1,6 +1,6 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-function even_or_odd(number){
+export function evenOrOdd(number){
   if(number === 0){
     return "Even";
     } else if (number % 2 === 0){
