0ff4d5674c63ad99dbb5e3d006e2a5d64207e58d
Troy Budd <troy_budd95@msn.com>
2020-01-31 08:57:31 +0000

    TB - Moved files

---
+++ Javascript/8kyu/evenOrOdd/evenOrOddd.test.js
@@ -0,0 +1,10 @@
+import { evenOrOdd } from './evenOrOdd'
+
+test('Given an odd number, it should return Odd', () => {
+    expect(evenOrOdd(1)).toBe('Odd')
+})
+
+test('Given an even number, it should return Even', () => {
+    expect(evenOrOdd(2)).toBe('Even')
+})
+
