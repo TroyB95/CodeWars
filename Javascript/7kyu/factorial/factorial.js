b4dcc90121f94346e64d4a7ede18ca245877dc0f
Troy Budd <troy_budd95@msn.com>
2020-02-04 09:46:37 +0000

    TB - Moved files into folder

--- Javascript/7kyu/factorial/factorial.js
+++ Javascript/7kyu/factorial/factorial.js
@@ -4,7 +4,7 @@
 
 // More details about factorial can be found here.
 
-function factorial(n) {
+export default function factorial(n) {
     if(n < 0 || n > 12) {
       throw new RangeError("The argument must be between 0 and 12") 
     } 
