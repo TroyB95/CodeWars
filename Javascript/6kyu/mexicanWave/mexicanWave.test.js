25445525df6b805fe813717b350a9402e09e4034
Troy Budd <troy_budd95@msn.com>
2020-02-10 09:02:30 +0000

    TB - Made testd more specific.

--- Javascript/6kyu/mexicanWave/mexicanWave.test.js
+++ Javascript/6kyu/mexicanWave/mexicanWave.test.js
@@ -1,6 +1,7 @@
 import { wave } from './mexicanWave';
 
 test('should format chars to form wave', () => {
-    expect(wave('hello')).toContain("Hello", "hEllo", "heLlo", "helLo", "hellO")
-
+    expect(wave('hello')).toEqual(["Hello", "hEllo", "heLlo", "helLo", "hellO"])
+    expect(wave('wave')).toEqual(["Wave", "wAve", "waVe", "wavE"])
+    expect(wave('wave')).not.toEqual(["wAve", "Wave", "wavE", "wavE"])
 })
